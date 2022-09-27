8cd4b1cafb3c0951732ed3acda45b417dd643bf1
Patricio Moracho <pm@marval.com>
2022-09-27 12:54:28 -0300

    feat: Agregamos un remover (por ahora solo lista)

--- gso/remover.py
+++ gso/remover.py
@@ -1,7 +1,9 @@
 import pyodbc
 import os
+import glob
 from pathlib import Path
 from gso.tabulate import tabulate
+from gso.exporter import get_objects, obj_type
 
 from progressbar import ProgressBar
 from progressbar import FormatLabel
@@ -10,19 +12,21 @@ from progressbar import Bar
 from progressbar import RotatingMarker
 from progressbar import ETA
 
-todo = """To do:
-1. Recoremos las carpeta del repositorio de forma recursiva
-2. Por cada pat completo al archivo
-    * obtenemos, database, tipo de objeto, owner, nombre del objeto
-    * Por cada database
-        * Nos conectamos
-        * Verificamos si existe el objeto
-        * Si no existe en ninguno eliminamos el archivo
-"""
-
 def remove(cfg):
 
-    for file in glob.iglob(repo_path + '\\**\\*.sql', recursive=True):
-        partes = file.replace(repo_path + '\\', "").split("\\")
-        if len(partes) == 3:
+    objetos = get_objects(cfg, "*.*.*.*.*")
+
+    logicos = set()
+    for _, base, owner, obj, tipo, _, _, _, _, _, _ in objetos:
+        logicos.add(".".join([base.lower(), (obj_type[tipo]).lower(), owner, obj]))
+
+    fisicos = set()
+    for file in glob.iglob(cfg.export_path + '\\**\\*.sql', recursive=True):
+        partes = file.replace(cfg.export_path + '\\', "").split("\\")
+        if len(partes) == 3 :
             db, tipo_objeto, objeto  = partes
+            if ".sql" in objeto:
+                fisicos.add(".".join([db, tipo_objeto, objeto.replace(".sql", "")]))
+
+    print('Objetos eliminados de las bases de daatos:')
+    print(fisicos.difference(logicos))
